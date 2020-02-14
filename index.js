f01eefa983047d4fe6c7648faf24c9e775c9ce35
André Ferreira <mrawesomeferreira@gmail.com>
2020-02-14 11:48:54 +0000

    solved lab

--- index.js
+++ index.js
@@ -11,9 +11,57 @@ mongoose
     useNewUrlParser: true,
     useUnifiedTopology: true
   })
+  .then(self => {
+    console.log(`Connected to the database: "${self.connections[0].name}"`);
+    return self.connection.dropDatabase();
+  })
+  // Run your code here, after you have insured that the connection was made
   .then(() => {
-    console.log(`Connected to the database: "${x.connections[0].name}"`);
-    // Run your code here, after you have insured that the connection was made
+    return Recipe.create({
+      title: 'Copycat Recipe',
+      level: 'Amateur Chef',
+      ingredients: [
+        '1/2 cup rice vinegar',
+        '5 tablespoons honey',
+        '1/3 cup soy sauce (such as Silver Swan®)',
+        '1/4 cup Asian (toasted) sesame oil',
+        '3 tablespoons Asian chili garlic sauce',
+        '3 tablespoons minced garlic',
+        'salt to taste',
+        '8 skinless, boneless chicken thighs'
+      ],
+      cuisine: 'Asian',
+      dishType: 'Dish',
+      image: 'https://images.media-allrecipes.com/userphotos/720x405/815964.jpg',
+      duration: 40,
+      creator: 'Chef LePapu'
+    });
+  })
+  .then(recipeDoc => {
+    console.log(recipeDoc.title);
+  })
+  .then(() => {
+    return Recipe.insertMany(data);
+  })
+  .then(multiRecipeDoc => {
+    multiRecipeDoc.forEach(element => {
+      console.log(element.title);
+    });
+  })
+  .then(() => {
+    return Recipe.findOneAndUpdate({ title: 'Rigatoni alla Genovese' }, { duration: 100 });
+  })
+  .then(() => {
+    console.log('Successfully updated!');
+  })
+  .then(() => {
+    return Recipe.findOneAndDelete({ title: 'Carrot Cake' });
+  })
+  .then(() => {
+    console.log('Successfully deleted!');
+  })
+  .then(() => {
+    return mongoose.disconnect();
   })
   .catch(error => {
     console.error('Error connecting to the database', error);
